99eb8abe35e5e78aa9bcf5d09363d7320ed4cf90
ReiNanaji <36332594+ReiNanaji@users.noreply.github.com>
2018-02-16 11:31:16 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,6 +1,24 @@
 '''
-Interesting functions:
-
 DataFrame.describe: give descriptive statistics
+DataFrame.sort_index 
+DataFrame.sort_values
 
+  Selection/Getting/Setting:
+  By Label:        .loc, .at (fast access)
+  By Position      .iloc, .iat (fast access)
+  (Boolean indexing .isin)
+
+  Apply:
+  
+  Merge/Join:
+  concat/merge/append
+  
+  Grouping:
+  groupby and apply functions
+  
+  Reshaping:
+  stack/unstack/pivot_table
+  
+  Time series:
+  resampling: with an arithmetic average
 '''
